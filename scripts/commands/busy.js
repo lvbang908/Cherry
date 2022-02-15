9e4764051f72931b8e1badc2fa1c839c84ada145
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-02-15 20:08:47 +0700

    Fix tương thích với lệnh all

--- scripts/commands/busy.js
+++ scripts/commands/busy.js
@@ -1,6 +1,6 @@
 module.exports.info = {
 	name: "busy",
-	version: "1.0.0",
+	version: "1.0.1",
 	permissions: 1,
 	author: {
 		name: "Henry",
@@ -42,7 +42,7 @@ module.exports.handleEvents = async function({ api, event, multiple, Users }) {
         }, messageID);
     }
 
-    if (!mentions || Object.keys(mentions).length == 0) return;
+    if (!mentions || Object.keys(mentions).length == 0 || senderID == api.getCurrrentUserID()) return;
     
     for (const [ID, name] of Object.entries(mentions)) {
         if (busyList.has(ID)) {

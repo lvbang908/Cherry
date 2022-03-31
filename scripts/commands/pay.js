3ce4bac809d52de3142474a8ab033e14baafed98
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-04-01 00:17:09 +0700

    New & fix

--- scripts/commands/pay.js
+++ scripts/commands/pay.js
@@ -1,6 +1,6 @@
 module.exports.info = {
 	name: "pay",
-    version: "1.0.0",
+    version: "1.1.0",
     permissions: 1,
     author: {
         name: "Henry",
@@ -19,28 +19,21 @@ module.exports.info = {
 
 module.exports.run = async({ event, api, args, Others }) => {
     var { senderID, threadID, messageID, mentions } = event;
-    const senderMoney = (await Others.getData(senderID)).money;
-    var mention = Object.keys(mentions);
-    if (mention.length == 0) return api.sendMessage("Bạn cần tag người nhận tiền.", threadID);
-    var mentionMoney = (await Others.getData(mention)).money;
-    var moneyPay = parseInt(args[args.length - 1]);
-    let Ry = parseInt('100005548624106');
-
-
-    if (isNaN(moneyPay) || moneyPay < 0) {
-        return api.sendMessage("Vui lòng chỉ nhập tiền chuyển là số và là số nguyên dương.", threadID, messageID);
-    } else if (!moneyPay) {
-        return api.sendMessage("Bạn cần nhập số tiền cần chuyển.", threadID, messageID);
-    } else if (Ry == senderID) {
-        return api.sendMessage(`Bạn đã chuyển ${moneyPay} coin cho ${mentions[mention].replace("@", "")}.`, threadID, async() => {
-            await Others.setData(mention, { coin: mentionMoney + moneyPay });
-        }, messageID)
-    } else if (senderMoney < moneyPay) {
-        return api.sendMessage("Số tiền bạn nhập lớn hơn số dư của bạn.", threadID, messageID);
-    } else {
-        return api.sendMessage(`Bạn đã chuyển ${moneyPay} coin cho ${mentions[mention].replace("@", "")}.`, threadID, async() => {
-            await Others.setData(mention, { coin: mentionMoney + moneyPay });
-            await Others.setData(senderID, { coin: senderMoney - moneyPay });
-        }, messageID)
+    var { coin: senderMoney } = await Others.getData(senderID);
+    var data = [], msg = '';
+    if (Object.keys(mentions).length == 0) return api.sendMessage('Bạn cần tag người nhận tiền.', threadID, messageID);
+    for (var [id, name] of Object.entries(mentions)) {
+        var { coin: mentionMoney } = await Others.getData(id);
+        data.push({ ID: id, name: name.replace("@", ""), coin: mentionMoney });
+        args = args.filter(item => item != name);
     }
+    var moneyPay = args;
+    if (isNaN(moneyPay) || moneyPay < 0 || !moneyPay) return api.sendMessage("Số tiền được chuyển phải là số và là số nguyên dương.", threadID, messageID);
+    if (senderMoney < moneyPay || Object.keys(mentions).length * moneyPay > senderMoney) return api.sendMessage('Số tiền bạn chuyển cho người khác phải nhỏ hơn hoặc bằng số dư của bạn.', threadID, messageID);
+    for (var i of data) {
+        await Others.setData(i.ID, { coin: i.coin + moneyPay });
+        await Others.setData(senderID, { coin: senderMoney - moneyPay });
+        msg += `- Bạn đã chuyển cho ${i.name} ${moneyPay} coin.`
+    }
+    return api.sendMessage(msg, threadID, messageID);
 }

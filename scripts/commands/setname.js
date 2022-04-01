f033d57450b14e487c4ad4bc33132ba76465730d
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-04-01 12:54:54 +0700

    Update setname.js

--- scripts/commands/setname.js
+++ scripts/commands/setname.js
@@ -1,6 +1,6 @@
 module.exports.info = {
 	name: "setname",
-    version: "1.0.0",
+    version: "1.0.2",
     permissions: 1,
     author: {
         name: "Henry",
@@ -19,15 +19,15 @@ module.exports.info = {
 
 module.exports.run = async function({ api, event, args }) {
 	const { threadID, senderID, mentions } = event, ID = [];
-	if (!mentions) {
+	if (Object.keys(mentions).length == 0) {
         var name = args.join(" ")
         return api.changeNickname(name, threadID, senderID);
     }
+    var nickname = args.join(" ");
     for (var [id, name] of Object.entries(mentions)) {
         ID.push(id)
-        args = args.filter(item => item != name);
+        nickname = nickname.replace(name, "");
     }
-    var nickname = args.join(" ");
     for (var i of ID) {
         api.changeNickname(nickname, threadID, i)
     }

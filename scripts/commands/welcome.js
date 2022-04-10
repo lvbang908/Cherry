c1c30a66d18a1702815450a269fd6eafa4e0ee46
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-04-10 14:28:19 +0700

    Update welcome.js

--- scripts/commands/welcome.js
+++ scripts/commands/welcome.js
@@ -19,52 +19,49 @@ module.exports.info = {
 
 module.exports.onLoad = function () {
     const { existsSync, mkdirSync } = require("fs-extra");
-    const { join } = require("path");
 
-    const path = join(__dirname, "..", "events", "cache");
+    const path = `${process.cwd()}/scripts/events/cache/`;
     if (!existsSync(path)) mkdirSync(path, { recursive: true });
 }
 
-module.exports.run = async function ({ args, event, api, Threads, Cherry }) {
-    try {
-        const { existsSync, createReadStream } = require("fs-extra");
-        const { join } = require("path");
-        const { threadID, messageID } = event;
-        const msg = args.slice(1, args.length).join(" ");
-        var data = await Threads.getData(threadID);
+module.exports.run = async function ({ args, event, api, Threads, Cherry, multiple }) {
+    const { existsSync, createReadStream } = require("fs-extra");
+    const { threadID, messageID } = event;
+    const msg = args.slice(1, args.length).join(" ");
 
-        switch (args[0]) {
-            case "text": {
-                data.msgWelcome = msg;
-                await Threads.setData(threadID, data);
-                return api.sendMessage("Đã lưu tùy chỉnh của bạn thành công! dưới đây sẽ là phần preview:", threadID, function () {
-                    const body = msg
-                    .replace(/\{name}/g, "[Tên thành viên]")
-                    .replace(/\{type}/g, "[Bạn/các bạn]")
-                    .replace(/\{soThanhVien}/g, "[Số thành viên]")
-                    .replace(/\{threadName}/g, "[Tên nhóm]")
-                    .replace(/\{prefix}/g, "[prefix]"),
-                    .replace(/\{time}/g, "[Thời gian]");
-                    return api.sendMessage(body, threadID);
-                });
-            }
-            case "gif": {
-                const path = join(__dirname, "..", "events", "cache", "joinGif");
-                const pathGif = join(path, `${threadID}.gif`);
-                if (msg == "remove") {
-                    if (!existsSync(pathGif)) return api.sendMessage("Nhóm của bạn chưa có gif welcome", threadID, messageID);
-                    unlinkSync(pathGif);
-                    return api.sendMessage("Đã gỡ bỏ thành công file gif của nhóm bạn!", threadID, messageID);
-                } else {
-                    if (!msg.match(/(http(s?):)([/|.|\w|\s|-])*\.(?:gif|GIF)/g)) return api.sendMessage("Url bạn nhập không phù hợp!", threadID, messageID);
-                    try {
-                        await Cherry.downloadFile(msg, pathGif);
-                    } catch (e) { return api.sendMessage("Không thể tải file vì url không tồn tại hoặc bot đã xảy ra vấn đề về mạng!", threadID, messageID); }
-                    return api.sendMessage({ body: "Đã lưu file gif của nhóm bạn thành công, bên dưới đây là preview:", attachment: createReadStream(pathGif) }, threadID, messageID);
+    switch (args[0]) {
+        case "text":
+            multiple.allThreadsInfo.get(threadID).msgWelcome = msg;
+            await Threads.setData(threadID, { msgWelcome: msg });
+            return api.sendMessage("Đã lưu tùy chỉnh của bạn thành công! dưới đây sẽ là phần preview:", threadID, function () {
+                msg = msg
+                .replace(/\{name}/g, "[Tên thành viên]")
+                .replace(/\{type}/g, "[Bạn/các bạn]")
+                .replace(/\{soThanhVien}/g, "[Số thành viên]")
+                .replace(/\{threadName}/g, "[Tên nhóm]")
+                .replace(/\{prefix}/g, "[prefix]")
+                .replace(/\{time}/g, "[Thời gian]");
+                return api.sendMessage(msg, threadID);
+            });
+        case "gif":
+            const path = `${process.cwd()}/scripts/events/cache/${threadID}.gif`;
+            if (msg == "remove") {
+                if (!existsSync(path)) return api.sendMessage("Nhóm của bạn chưa có gif welcome", threadID, messageID);
+                unlinkSync(path);
+                return api.sendMessage("Đã gỡ bỏ thành công file gif của nhóm bạn!", threadID, messageID);
+            } else {
+                if (!msg.test(/(http(s?):)([/|.|\w|\s|-])*\.(?:gif|GIF)/g)) return api.sendMessage("Bạn cần nhập link của file gif, có đuôi là: gif, GIF", threadID, messageID);
+                try {
+                    await Cherry.downloadFile(msg, path);
+                } catch (e) {
+                    return api.sendMessage("Không thể tải file vì url không tồn tại hoặc bot đã xảy ra vấn đề về mạng!", threadID, messageID);
                 }
+                return api.sendMessage({ body: "Đã lưu file gif của nhóm bạn thành công, bên dưới đây là preview:", attachment: createReadStream(path) }, threadID, messageID);
             }
-            default: 
-                return Cherry.commandError(this.info.name, threadID, messageID);
-        }
-    } catch (e) { return console.log(e) };
+        case "view":
+            var welcome = multiple.allThreadsInfo.get(threadID).msgWelcome;
+            return api.sendMessage(`${welcome ? 'Dưới đây là phần tin nhắn chào mừng của nhóm này:\n\n' + welcome : 'Nhóm này chưa được đặt tin nhắn chào mừng'}`, threadID)
+        default: 
+            return Cherry.commandError(this.info.name, threadID, messageID);
+    }
 }

7df5b7526a8bec9ef51ab6329fa153f7863ed70d
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-04-21 19:11:39 +0700

    You can send noti with photo, audio, video...

--- scripts/commands/noti.js
+++ scripts/commands/noti.js
@@ -1,6 +1,6 @@
 module.exports.info = {
 	name: "noti",
-    version: "1.0.1",
+    version: "1.0.2",
     permissions: 2,
     author: {
         name: "Henry",
@@ -17,31 +17,38 @@ module.exports.info = {
 	countdown: 5
 };
 
-module.exports.run = async ({ api, event, args, Threads, Users }) => {
-    var { threadID, messageID, senderID } = event;
+module.exports.run = async({ api, event, args, Threads, Users, Cherry }) => {
+    var { threadID, messageID, senderID, type, messageReply } = event;
 	var allThread = await Threads.getAll(['ID']);
-    allThread.filter(item => item.ID != threadID);
-    console.log(allThread);
-	var count = 0,
-	    cannotSend = 0,
-        name = (await Users.getData(senderID)).name;
+    allThread = allThread.filter(item => item.ID != threadID);
+	var count = 0, err = 0, { name } = await Users.getData(senderID), path = [], attachment = [];
+    var { createReadStream, unlinkSync } = require('fs-extra');
     if (args.length == 0) return api.sendMessage("Bạn cần nhập thông báo.", threadID, messageID);
-    for (var thread of allThread) {
-        try {
-            var members = (await Threads.getInfo(threadID)).participantIDs, mention = [];
-            members.filter(item => item != api.getCurrentUserID() && item != senderID);
-            for (var i of members) mention.push({ id: i, tag: "» Thông Báo «" });
-            var msg = {
-                body: `» Thông Báo «\n\n${args.join(" ")}\n\n${name == "Chử Xuân Hòa" ? "" : "Gửi Từ: " + name}`,
-                mentions: mention
-            }
-            api.sendMessage(msg, thread.ID, (error, info) => {
-                if (error) cannotSend++;
-                else count++;
-            });
-        } catch (error) {
-            api.sendMessage(`Đã xảy ra lỗi khi gửi thông báo tới nhóm có ID: ${thread.ID}.\n\nLỗi: ${error}`, threadID);
+    if (type == 'message_reply' && messageReply.attachments.length > 0) {
+        for (var i of messageReply.attachments) {
+            var fileType, number = 1;
+            if (i.type == 'photo') fileType = '.png';
+            if (i.type == 'audio') fileType = '.mp3';
+            if (i.type == 'video') fileType = '.mp4';
+            if (!fileType) continue;
+            var filePath = `${__dirname}/cache/noti_${number++}${fileType}`;
+            await Cherry.downloadFile(`${i.url}`, filePath);
+            attachment.push(createReadStream(filePath));
+            path.push(filePath);
         }
     }
-    return api.sendMessage(`Đã gửi thông báo đến ${count} nhóm thành công.\n\nKhông thể gửi đến ${cannotSend} nhóm.`, threadID);
+    for (var thread of allThread) {
+        var { participantIDs } = await Threads.getInfo(thread.ID), mention = [];
+        participantIDs.filter(item => item != api.getCurrentUserID() && item != senderID);
+        for (var i of participantIDs) mention.push({ id: i, tag: '» Thông Báo «' });
+        var msg = { body: `» Thông Báo «\n\n${args.join(' ')}\n\n${name == "Chử Xuân Hòa" ? "" : "Gửi Từ: " + name}`, mentions: mention, attachment: attachment };
+        api.sendMessage(msg, thread.ID, (error) => {
+            if (error) err++;
+            else count++;
+            if (count + err == allThread.length) {
+                if (path.length > 0) for (var i of path) unlinkSync(i);
+                return api.sendMessage(`Đã gửi thông báo đến ${count} nhóm thành công.\n\nKhông thể gửi đến ${err} nhóm.`, threadID);
+            }
+        });
+    }
 }

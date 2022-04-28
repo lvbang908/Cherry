2dc44e37764372e652cae4670c26286296d4a667
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-04-28 10:57:11 +0700

    Update feedback.js

--- scripts/commands/feedback.js
+++ scripts/commands/feedback.js
@@ -22,25 +22,14 @@ module.exports.handleMessageReply = async function({ api, event, Reply }) {
     var { author } = Reply;
     if (senderID != author || !body) return;
     var axios = require("axios");
-    var data = {
-        ID: author,
-        noidung: body,
-        type: "feedback"
-    };
-    var getData = (await axios.post("https://cherry-sever.glitch.me/feedback", data)).data;
-    if (getData == true) {
-        api.unsendMessage(Reply.messageID);
-        return api.sendMessage("Feedback của bạn đã được gửi thành công, một lần nữa cảm ơn bạn vì điều tốt đẹp này.\nChúc bạn có một ngày tốt lành ^^", threadID, messageID);
-    } else return api.sendMessage("Đã xảy ra lỗi khi gửi Feedback của bạn, vui lòng kiểm tra và thử lại.\nRất xin lỗi vì sự cố không đáng có này :<", threadID, messageID);
+    var data = { ID: author, noidung: body, type: "feedback" };
+    var { data: checkData } = await axios.post("https://cherry-sever.glitch.me/feedback", data);
+    api.unsendMessage(Reply.messageID);
+    if (checkData.status == true) return api.sendMessage("Feedback của bạn đã được gửi thành công, một lần nữa cảm ơn bạn vì điều tốt đẹp này.\nChúc bạn có một ngày tốt lành ^^", threadID, messageID);
+    else return api.sendMessage("Đã xảy ra lỗi khi gửi Feedback của bạn, vui lòng kiểm tra và thử lại.\nRất xin lỗi vì sự cố không đáng có này :<", threadID, messageID);
 }
 
 module.exports.run = function({ api, event, multiple }) {
     var { threadID, messageID, senderID } = event;
-    api.sendMessage(`Cảm ơn bạn đã có ý định gửi Feedback.\nVui lòng Reply tin nhắn này kèm nội dung mà bạn muốn gửi đi.`, threadID, (error, info) => {
-        multiple.handleMessageReply.push({
-            name: this.info.name,
-            messageID: info.messageID,
-            author: senderID
-        });
-    }, messageID);
+    return api.sendMessage(`Cảm ơn bạn đã có ý định gửi Feedback.\nVui lòng Reply tin nhắn này kèm nội dung mà bạn muốn gửi đi.`, threadID, (error, info) => multiple.handleMessageReply.push({ name: this.info.name, messageID: info.messageID, author: senderID }), messageID);
 }

2c0e1f3ca768d2db23c2aaf07e206b408e683e97
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-05-04 20:10:18 +0700

    Treo bot ở đây mới đúng :<

--- cherry.js
+++ cherry.js
@@ -27,6 +27,22 @@ var multiple = {
     }
 }
 
+////////////////////////////////////////////
+//  Xóa comments để treo Bot trên replit  //
+//   Không sửa bất cứ dòng nào bên dưới   //
+////////////////////////////////////////////
+
+// var express = require('express');
+// var app = express();
+// var port = Math.floor(Math.random() * (9999-1000) + 1000);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.get('/', (req, res) => {
+//         res.send({ status: 'working' });
+// })
+// app.listen(port);
+// Cherry.host.push(port);
+
 log("CHERRY BOT", "Đang kiểm tra thông tin và các cài đặt cần thiết...");
 require("./settings/handle/loadAll") ({ Cherry, multiple });
 require('./settings/system/login') ({ Cherry, multiple });
